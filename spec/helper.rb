bda5612921c9051171c93142f981ba2c511de594
Larry Marburger <larry@marburger.cc>
2013-03-08 08:38:37 -0500

    Run focused specs

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,2 +1,8 @@
 require 'English'
 $LOAD_PATH.unshift File.expand_path('lib/guardian', File.expand_path('../..', __FILE__))
+
+RSpec.configure do |config|
+  config.filter_run :focused => true
+  config.run_all_when_everything_filtered = true
+  config.alias_example_to :fit, :focused => true
+end
