5b685ec0bafa3c399ddea312e4f1d155e0c89c7f
Larry Marburger <larry@marburger.cc>
2013-03-25 16:08:28 -0400

    Break apart log line in request spec

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -4,9 +4,29 @@ require 'guardian/request'
 
 describe Guardian::LogLine do
   let(:logger)   { stub(:logger, puts: nil) }
-  let(:log_line) { <<LINE }
-abc123 bucket-name [03/Feb/2013:20:23:01 +0000] 8.8.8.8 def456 ghi789 REST.GET.OBJECT items/abc123/file.jpg "GET /f.cl.ly/items/abc123/file.jpg?AWSAccessKeyId=ACCESSKEY&Expires=1359926569&Signature=SIGNATURE&response-content-disposition=attachment HTTP/1.1" 200 - 18946246 18946246 2243335 96 "http://getcloudapp.com" "Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-I9300 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" -
-LINE
+  let(:owner) { 'abc123' }
+  let(:bucket_name) { 'bucket-name' }
+  let(:access_time) { '[03/Feb/2013:20:23:01 +0000]' }
+  let(:remote_ip) { '8.8.8.8' }
+  let(:requestor) { 'def456' }
+  let(:request_id) { 'ghi789' }
+  let(:operation) { 'REST.GET.OBJECT' }
+  let(:key) { 'items/abc123/file.jpg' }
+  let(:request_uri) { '"GET /f.cl.ly/items/abc123/file.jpg?AWSAccessKeyId=ACCESSKEY&Expires=1359926569&Signature=SIGNATURE&response-content-disposition=attachment HTTP/1.1"' }
+  let(:http_status) { '200' }
+  let(:error_code) { '-' }
+  let(:bytes_sent) { '18946246' }
+  let(:object_size) { '18946246' }
+  let(:total_time) { '2243335' }
+  let(:turn_around_time) { '96' }
+  let(:referrer) { '"http://getcloudapp.com"' }
+  let(:user_agent) { '"Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-I9300 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"' }
+  let(:version_id) { '-' }
+  let(:log_line) {[ owner, bucket_name, access_time, remote_ip, requestor,
+                    request_id, operation, key, request_uri, http_status,
+                    error_code, bytes_sent, object_size, total_time,
+                    turn_around_time, referrer, user_agent, version_id
+                  ].join(' ') }
 
   describe '.record_log_line_from_log_file' do
     subject {
@@ -39,9 +59,7 @@ LINE
     end
 
     context 'without a request uri' do
-      let(:log_line) { <<LINE }
-abc123 bucket-name [03/Feb/2013:20:23:01 +0000] 8.8.8.8 def456 ghi789 REST.GET.OBJECT items/abc123/file.jpg - 200 - 18946246 18946246 2243335 96 "http://getcloudapp.com" "Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-I9300 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" -
-LINE
+      let(:request_uri) { '-' }
       it { should_not be_nil }
     end
   end
@@ -79,36 +97,28 @@ LINE
     end
 
     context 'without a key' do
-      let(:log_line) { <<LINE }
-abc123 bucket-name [03/Feb/2013:20:23:01 +0000] 8.8.8.8 def456 ghi789 REST.GET.OBJECT - "GET /f.cl.ly/items/abc123/file.jpg?AWSAccessKeyId=ACCESSKEY&Expires=1359926569&Signature=SIGNATURE&response-content-disposition=attachment HTTP/1.1" 200 - 18946246 18946246 2243335 96 "http://getcloudapp.com" "Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-I9300 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" -
-LINE
+      let(:key) { '-' }
       it 'has no key' do
         subject.key.should be_nil
       end
     end
 
     context 'without an http status' do
-      let(:log_line) { <<LINE }
-abc123 bucket-name [03/Feb/2013:20:23:01 +0000] 8.8.8.8 def456 ghi789 REST.GET.OBJECT items/abc123/file.jpg "GET /f.cl.ly/items/abc123/file.jpg?AWSAccessKeyId=ACCESSKEY&Expires=1359926569&Signature=SIGNATURE&response-content-disposition=attachment HTTP/1.1" - - 18946246 18946246 2243335 96 "http://getcloudapp.com" "Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-I9300 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" -
-LINE
+      let(:http_status) { '-' }
       it 'has no http status' do
         subject.http_status.should be_nil
       end
     end
 
     context 'with no bytes sent' do
-      let(:log_line) { <<LINE }
-abc123 bucket-name [03/Feb/2013:20:23:01 +0000] 8.8.8.8 def456 ghi789 REST.GET.OBJECT - "GET /f.cl.ly/items/abc123/file.jpg?AWSAccessKeyId=ACCESSKEY&Expires=1359926569&Signature=SIGNATURE&response-content-disposition=attachment HTTP/1.1" 200 - - 18946246 2243335 96 "http://getcloudapp.com" "Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-I9300 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" -
-LINE
+      let(:bytes_sent) { '-' }
       it 'has no key' do
         subject.bytes_sent.should be_nil
       end
     end
 
     context 'with a request uri containing a quote' do
-      let(:log_line) { <<LINE }
-abc123 bucket-name [03/Feb/2013:20:23:01 +0000] 8.8.8.8 def456 ghi789 REST.GET.OBJECT items/abc123/file.jpg%2522 "GET items/abc123/file.jpg" HTTP/1.1" 403 AccessDenied 231 - 37 - "http://getcloudapp.com" "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6 (FlipboardProxy/0.0.5; +http://flipboard.com/browserproxy)" -
-LINE
+      let(:request_uri) { '"GET items/abc123/file.jpg" HTTP/1.1"' }
       it { should_not be_nil }
     end
 
@@ -122,39 +132,29 @@ LINE
     end
 
     context 'without a request uri' do
-      let(:log_line) { <<LINE }
-abc123 bucket-name [03/Feb/2013:20:23:01 +0000] 8.8.8.8 def456 ghi789 REST.GET.OBJECT items/abc123/file.jpg - 200 - 18946246 18946246 2243335 96 "http://getcloudapp.com" "Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-I9300 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" -
-LINE
+      let(:request_uri) { '-' }
       it { should_not be_nil }
     end
 
     context 'with an empty, quoted referrer' do
-      let(:log_line) { <<LINE }
-abc123 bucket-name [03/Feb/2013:20:23:01 +0000] 8.8.8.8 def456 ghi789 REST.GET.OBJECT items/abc123/file.jpg "GET /f.cl.ly/items/abc123/file.jpg?AWSAccessKeyId=ACCESSKEY&Expires=1359926569&Signature=SIGNATURE&response-content-disposition=attachment HTTP/1.1" 200 - 18946246 18946246 2243335 96 "-" "Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-I9300 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" -
-LINE
+      let(:referrer) { '"-"' }
       it 'has no referrer' do
         subject.referrer.should be_nil
       end
     end
 
     context 'without a referrer' do
-      let(:log_line) { <<LINE }
-abc123 bucket-name [03/Feb/2013:20:23:01 +0000] 8.8.8.8 def456 ghi789 REST.GET.OBJECT items/abc123/file.jpg "GET /f.cl.ly/items/abc123/file.jpg?AWSAccessKeyId=ACCESSKEY&Expires=1359926569&Signature=SIGNATURE&response-content-disposition=attachment HTTP/1.1" 200 - 18946246 18946246 2243335 96 - "Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-I9300 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" -
-LINE
+      let(:referrer) { '-' }
       it { should_not be_nil }
     end
 
     context 'without a user agent' do
-      let(:log_line) { <<LINE }
-abc123 bucket-name [03/Feb/2013:20:23:01 +0000] 8.8.8.8 def456 ghi789 REST.GET.OBJECT items/abc123/file.jpg "GET /f.cl.ly/items/abc123/file.jpg?AWSAccessKeyId=ACCESSKEY&Expires=1359926569&Signature=SIGNATURE&response-content-disposition=attachment HTTP/1.1" 200 - 18946246 18946246 2243335 96 "http://getcloudapp.com" - -
-LINE
+      let(:user_agent) { '-' }
       it { should_not be_nil }
     end
 
     context 'with a user agent containing a quote' do
-      let(:log_line) { <<LINE }
-abc123 bucket-name [03/Feb/2013:20:23:01 +0000] 8.8.8.8 def456 ghi789 REST.GET.OBJECT items/abc123/file.jpg "GET /f.cl.ly/items/abc123/file.jpg?AWSAccessKeyId=ACCESSKEY&Expires=1359926569&Signature=SIGNATURE&response-content-disposition=attachment HTTP/1.1" 200 - 18946246 18946246 2243335 96 "http://getcloudapp.com" ""User Agent"" -
-LINE
+      let(:user_agent) { '""User Agent""' }
       it { should_not be_nil }
     end
   end
